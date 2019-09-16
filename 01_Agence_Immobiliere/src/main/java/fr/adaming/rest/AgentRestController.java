8b082e72736d931eaba1204133a706526bda7546
clement <clement.journouxlapp@gmail.com>
2019-09-16 21:39:36 +0200

    ajout controller agent (a verifier demain matin)

---
+++ 01_Agence_Immobiliere/src/main/java/fr/adaming/rest/AgentRestController.java
@@ -0,0 +1,25 @@
+package fr.adaming.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.adaming.entity.Agent;
+import fr.adaming.service.IAgentService;
+
+@RestController
+@RequestMapping("/agent")
+@CrossOrigin(origins= {"http://localhost:4200"})
+public class AgentRestController {
+	
+	@Autowired
+	IAgentService agentService;
+
+	@RequestMapping(method=RequestMethod.POST, value="/isExist", produces="application/json", consumes="application/json")
+	public Agent isExistRest(@RequestBody Agent a) {
+		return agentService.isExist(a);
+	}
+}
